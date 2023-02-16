57230992ca54fc78f1a8283737a00e924290afd6
ShevchenkoDmytriy <dimasevcenko506@gmail.com>
2023-02-16 15:56:57 +0200

    c#

---
+++ WpfApp14/WpfApp14/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp14
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            using(ApplicationContext db = new ApplicationContext())
+            {
+                User tom = new User { Name="Tom", Age =23,Surname="qwfqwfqf"/*,Email="gmail.com"*/};
+                db.Users.Add(tom);
+                db.SaveChanges();
+                var users = db.Users.ToList();
+                foreach(var user in users) 
+                {
+                    Box.Text += $"{user.Name} - {user.Age} \n";
+                }
+            }
+        }
+    }
+}
